07ba35c0f1f8bacc2d4a1cb33bb4d6976f5d78b2
frostie <mattwiner04@gmail.com>
2024-10-28 09:35:13 -0700

    1

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
+from flask import *
+app = Flask(__name__)
 def createStorage():
     directories = ["storage", "storage/data", "storage/music", "storage/tv", "storage/movies", "storage/photos"]
     for directory in directories:
         if not os.path.exists(directory):
             os.mkdir(directory)
     print("Done Creating Storages")
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 if __name__ == "__main__":
     createStorage()
+    app.run(
+        port=9000,
+        debug=True,
+    )
